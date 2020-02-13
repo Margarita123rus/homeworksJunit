6e6957627e869533a83f35a1aa063f06f4927cbb
Margarita123rus <margarita23_87@mail.ru>
2020-02-13 21:09:21 +0300

    добавление 2 автотестов

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -7,12 +7,34 @@ import static org.junit.jupiter.api.Assertions.*;
 class CashbackHackServiceTest {
 
     @Test
-    void mustOfferToBuyUpTo1000() {
+    void must1OfferToBuyUpTo1000() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 900;
 
         int actual = cashbackHackService.remain(amount);
         int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void must2OfferToBuyUpTo1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void must3OfferToBuyUpTo1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1100;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 900;
 
         assertEquals(expected, actual);
     }
